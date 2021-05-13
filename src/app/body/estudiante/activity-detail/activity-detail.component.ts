9fa20dbe75c7fd81b451070146c68e1a9d666b80
Andres Ruiz <ar2224518@gmail.com>
2021-05-13 00:58:25 -0500

    cargar archivo por clic

--- src/app/body/estudiante/activity-detail/activity-detail.component.ts
+++ src/app/body/estudiante/activity-detail/activity-detail.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityDetailComponent implements OnInit {
  
-  listaArchivos:string[] =['archivo.pdf', 'archivo.docx', 'archivo.xls'];  
-  listaEnlaces:string[] =['https://www.youtube.com/watch?v=qhAbg_eVXSw&list=RDqhAbg_eVXSw&start_radio=1', 'https://www.youtube.com/watch?v=8wGN7D03Nho&list=RDqhAbg_eVXSw&index=4',];  
+  listaArchivos:string[]  = ['archivo.pdf', 'archivo.docx', 'archivo.xls'];  //Este array es de prueba
+  listaEnlaces:string[]   = [
+    'https://www.youtube.com/watch?v=qhAbg_eVXSw&list=RDqhAbg_eVXSw&start_radio=1', 
+    'https://www.youtube.com/watch?v=8wGN7D03Nho&list=RDqhAbg_eVXSw&index=4',];  //Este aray es de prueba
+  archivoCargado:string[] = [];
   aniadirTarea:boolean = false;
   constructor() { }
 
@@ -17,6 +20,14 @@ export class ActivityDetailComponent implements OnInit {
   }
   cancelar():void{
     this.aniadirTarea = false;    
+  }
+
+  getArchivo(e){
+    const d = e.target.files[0].name;    
+    console.log(d);    
+    if(d!=''){
+      this.archivoCargado.push(d);        
+    }
   }
 
   ngOnInit(): void {}
