8efc47ca644854896461f8db011c0fb5a3f04fad
andres ruiz <amestr367@gmail.com>
2021-09-06 14:52:08 -0500

    añadir archivo

--- src/app/protected/teacher/agregar-actividad/agregar-actividad.component.ts
+++ src/app/protected/teacher/agregar-actividad/agregar-actividad.component.ts
@@ -6,7 +6,7 @@ import { ServiceDocenteService } from '../../../services/service-docente.service
 import { BodyGrupo, BodyAsignatura, BodyActividadRespons3 } from '../../../interface/inteface_tracher';
 
 import * as _moment from 'moment';
-const moment = _moment; 
+const moment = _moment;
 @Component({
   selector: 'app-agregar-actividad',
   templateUrl: './agregar-actividad.component.html',
@@ -17,7 +17,7 @@ export class AgregarActividadComponent implements OnInit {
   items=['Home', 'Actividades', 'Cursos'];
   private id_grado: string;
   private id_grupo:string;
-  private id_actividad:number;
+   id_actividad:string;
   aniadirTarea:boolean = false;
   private id_asignatura:string;
   archivoCargado:string[] = [];
@@ -28,10 +28,10 @@ export class AgregarActividadComponent implements OnInit {
 
 
   constructor(
-    private router: ActivatedRoute, 
+    private router: ActivatedRoute,
     private serviceup: UploadServiceService,
     private serviceDicente:ServiceDocenteService,
-    private fb: FormBuilder, 
+    private fb: FormBuilder,
     ) { }
 
     //esto se carga al inicar la pestaña
@@ -40,67 +40,67 @@ export class AgregarActividadComponent implements OnInit {
     //Obteniendo id
     this.id_grado = this.router.snapshot.paramMap.get('id_grado');
     this.id_grupo = this.router.snapshot.paramMap.get('id_grupo');
-  
 
-    
+
+
     //obteniendo grupo
     this.serviceDicente.getGrupo(this.id_grado, this.id_grupo).
                     subscribe(
                           (response:BodyGrupo) =>{
                                 console.log(response);
-                                
+
                             this.grado = response[0].grado;
                             this.posicion = response[0].posicion;
                           }
     );
-    //obteniendo asignatura                        
+    //obteniendo asignatura
     this.serviceDicente.getAsignatura(this.id_grupo)
                         .subscribe((response:BodyAsignatura)=>{
                           console.log(response);
                           console.log(response, '2222222222222');
-                          
+
                           this.id_asignatura = response[0].id_asignaturas;
                           this.asiganatura = response[0].nombre;
-                        });     
+                        });
   }
 
   formulario:FormGroup = this.fb.group({
     descripcion: ['',[Validators.required]],
     fechaLimite:['', [Validators.required]]
   });
- 
+
   crearActividad(){
     const {descripcion, fechaLimite} = this.formulario.value;
 
     const formatoActual = new Date(fechaLimite);
     const fecha_limite = moment(formatoActual).format('YYYY-MM-DD');
-      
+
       this.serviceDicente.CrearActvidad(fecha_limite, descripcion, this.id_grupo)
                           .subscribe((res:BodyActividadRespons3)=>{
-                             this.id_actividad = res[0].id_actividad 
-                            
+                             this.id_actividad = res[0].id_actividad
+
                           });
-                         
 
 
-    
-    
+
+
+
   }
 
 
   aniadir():void{
-    this.aniadirTarea = true;    
+    this.aniadirTarea = true;
     }
     cancelar():void{
-      this.aniadirTarea = false;    
+      this.aniadirTarea = false;
     }
-  
+
     getArchivo(e){
-      const d = e.target.files[0].name;    
+      const d = e.target.files[0].name;
       if(d!=''){
-        this.serviceup.updloadFile(e, this.id_grado, this.id_grupo, this.id_asignatura);
-        
-        this.archivoCargado.push(d);        
+        this.serviceup.updloadFile(e, this.id_grado, this.id_grupo, this.id_asignatura, this.id_actividad);
+
+      
       }
     }
 
@@ -108,5 +108,5 @@ export class AgregarActividadComponent implements OnInit {
 
     //Evento que se gatilla cuando el input de tipo archivo cambia
 
-    
+
 }
--- src/app/services/upload-service.service.ts
+++ src/app/services/upload-service.service.ts
@@ -1,6 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { finalize } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { ResponseInterface } from '../interface/inteface_tracher';
 
 
 @Injectable({
@@ -8,6 +12,7 @@ import { finalize } from 'rxjs/operators';
 })
 export class UploadServiceService {
 
+  private baseUrl:String  = environment.baseURL;
   private  id_docente:string = localStorage.getItem('userId');
   private fileName:string;
   private filePath:string;
@@ -18,33 +23,72 @@ export class UploadServiceService {
 
 
 
-  constructor( private  storage: AngularFireStorage) { }
+  constructor( 
+    private  storage: AngularFireStorage,
+    private http: HttpClient
+    ) { }
 
 
-  updloadFile(event,id_grado:string, id_grupo:string, id_asignatura:string ){
+  updloadFile(event,id_grado:string, id_grupo:string, id_asignatura:string, id_actividad:string ){
    
+
+ 
     const file = event.target.files[0];
     this.fileName = file.name;
+    this.size = `${file.size/1000}`;
+    this.format = file.name.split(".");
+
+    
+
     this.filePath =`${this.id_docente}/${id_grado}/${id_grupo}/${id_asignatura}/${this.fileName}`;
     const fileRef = this.storage.ref(this.filePath);
     const upload = this.storage.upload(this.filePath, file);
-    upload.snapshotChanges()
+
+    
+    return upload.snapshotChanges()
           .pipe( 
             finalize(()=>{
                 fileRef.getDownloadURL()
-                       .subscribe(url=>{
-                         console.log(url);
+                       .subscribe((url)=>{
+                         this.url = url;
+                         const urls = `${this.baseUrl}/aniadirArchivo`;  
+      
+                         const body = {
                          
+                           id_actividad,
+                           peso: this.size,
+                           formato:this.format[1],
+                           nombre:this.format[0],
+                           ruta:this.url,
+                           tipo_archivo:'guia'
+                        
+                       };
+                   
+                       return  this.http.post<ResponseInterface>(urls,body)
+                                         .pipe(
+                                           map(res=>{
+                                             return res.body;
+                                           }),
+                                           catchError(e=>of(e))
+                                         ).subscribe(e=>{
+                                           console.log(e);
+                                           
+                                         });
+                         
+
                        })
-                  
             })
           ).subscribe(
-            response =>{
-              console.log(response, 'response');
-              
-            }
-          );
 
+
+              res=>{
+                console.log('ssss', this.url);
+                
+              }
+         
+          );
+      
+              
 
   }
 
